495d3d770c13bbdbb426151c51781510b80312a6
Relkondo <samuel.coron@hotmail.fr>
2022-01-14 12:24:33 +0100

    working on trackPage

--- music_room_app/lib/spotify_library/track/track_manager.dart
+++ music_room_app/lib/spotify_library/track/track_manager.dart
@@ -1,28 +1,78 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:music_room_app/home/models/playlist.dart';
 import 'package:music_room_app/home/models/track.dart';
+import 'package:music_room_app/widgets/spotify_constants.dart';
+import 'package:spotify_sdk/models/connection_status.dart';
+import 'package:spotify_sdk/spotify_sdk.dart';
+import 'package:logger/logger.dart';
 
 class TrackManager with ChangeNotifier {
-  TrackManager({
-    required this.playlist,
-    required this.track});
+  TrackManager({required this.playlist, required this.track});
 
-  Playlist playlist;
-  Track track;
-  bool isLiked = false;
+  final Playlist playlist;
+  final Track track;
+  bool isAdded = false;
   bool isPlayed = false;
+  bool isConnected = false;
+  bool isLoading = false;
   Duration duration = const Duration();
   Duration position = const Duration();
+  final Logger _logger = Logger(
+    printer: PrettyPrinter(
+      methodCount: 2,
+      // number of method calls to be displayed
+      errorMethodCount: 8,
+      // number of method calls if stacktrace is provided
+      lineLength: 120,
+      colors: true,
+      printEmojis: true,
+      printTime: true,
+    ),
+  );
 
-  toggleLike() {
-    isLiked = isLiked == true ? false : true;
-    notifyListeners();
+  Future<String> _getAuthenticationToken() async {
+    try {
+      var authenticationToken = await SpotifySdk.getAuthenticationToken(
+          clientId: spotifyClientId,
+          redirectUrl: spotifyRedirectUri,
+          scope: 'app-remote-control, '
+              'user-modify-playback-state, '
+              'playlist-read-private, '
+              'playlist-modify-public,user-read-currently-playing');
+      setStatus('Got a token: $authenticationToken');
+      return authenticationToken;
+    } on PlatformException catch (e) {
+      setStatus(e.code, message: e.message);
+      rethrow;
+    } on MissingPluginException {
+      setStatus('not implemented on this platform');
+      rethrow;
+    }
   }
 
-  togglePlay() {
-    isPlayed = isPlayed == true ? false : true;
-    notifyListeners();
+  Future<void> connectToSpotifyRemote() async {
+    try {
+      updateLoading(true);
+      await _getAuthenticationToken();
+      var result = await SpotifySdk.connectToSpotifyRemote(
+          clientId: spotifyClientId, redirectUrl: spotifyRedirectUri);
+      setStatus(result
+          ? 'connect to spotify successful'
+          : 'connect to spotify failed');
+      updateLoading(false);
+    } on PlatformException catch (e) {
+      updateLoading(false);
+      setStatus(e.code, message: e.message);
+    } on MissingPluginException {
+      updateLoading(false);
+      setStatus('not implemented');
+    }
   }
+
+  toggleLike() => updateAdded(isAdded == true ? false : true);
+
+  togglePlay() => updatePlayed(isPlayed == true ? false : true);
 
   Widget? returnImage() {
     if (track.album != null) {
@@ -57,5 +107,46 @@ class TrackManager with ChangeNotifier {
     // set to the received second
   }
 
+  StreamBuilder<ConnectionStatus> getStreamBuilder(Widget child) {
+    return StreamBuilder<ConnectionStatus>(
+      stream: SpotifySdk.subscribeConnectionStatus(),
+      builder: (context, snapshot) {
+        isConnected= false;
+        var data = snapshot.data;
+        if (data != null) {
+          isConnected = data.connected;
+        }
+        return child;
+      },
+    );
+  }
 
+  void updateAdded(bool isAdded) => _updateWith(isAdded: isAdded);
+
+  void updatePlayed(bool isPlayed) => _updateWith(isPlayed: isPlayed);
+
+  void updateConnected(bool isConnected) =>
+      _updateWith(isConnected: isConnected);
+
+  void updateLoading(bool isLoading) => _updateWith(isLoading: isLoading);
+
+  void updatePosition(Duration position) => _updateWith(position: position);
+
+  void updateDuration(Duration duration) => _updateWith(duration: duration);
+
+  void _updateWith(
+      {bool? isAdded, bool? isPlayed, bool? isConnected, bool? isLoading, Duration? position, Duration? duration}) {
+    this.isAdded = isAdded ?? this.isAdded;
+    this.isPlayed = isPlayed ?? this.isPlayed;
+    this.isConnected = isConnected ?? this.isConnected;
+    this.isLoading = isLoading ?? this.isLoading;
+    this.position = position ?? this.position;
+    this.duration = duration ?? this.duration;
+    notifyListeners();
+  }
+
+  void setStatus(String code, {String? message}) {
+    var text = message ?? '';
+    _logger.i('$code$text');
+  }
 }
--- music_room_app/lib/spotify_library/track/track_page.dart
+++ music_room_app/lib/spotify_library/track/track_page.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:music_room_app/home/models/playlist.dart';
 import 'package:music_room_app/home/models/track.dart';
 import 'package:music_room_app/spotify_library/track/track_manager.dart';
 import 'package:music_room_app/widgets/custom_appbar.dart';
 import 'package:provider/provider.dart';
+import 'package:spotify_sdk/models/connection_status.dart';
+import 'package:spotify_sdk/spotify_sdk.dart';
 
 class TrackPage extends StatefulWidget {
   const TrackPage(
@@ -49,30 +52,29 @@ class _TrackPageState extends State<TrackPage> {
 
   @override
   Widget build(BuildContext context) {
-    // SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    //   statusBarColor: Colors.transparent,
-    // ));
-    return Scaffold(
-      // appBar: customAppBar(
-      //     appText: 'Playing from ' + widget.playlist.name, context: context),
-      // backgroundColor: const Color(0xFFEFEFF4),
-      body: Container(
-        padding: const EdgeInsets.only(
-          left: 10,
-          right: 10,
-          top: 40,
-        ),
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            colors: [
-              Colors.blueGrey,
-              Colors.black87,
-            ],
-            end: Alignment.bottomCenter,
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
+    return manager.getStreamBuilder(
+      Scaffold(
+        body: Container(
+          padding: const EdgeInsets.only(
+            left: 10,
+            right: 10,
+            top: 40,
           ),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              colors: [
+                Colors.blueGrey,
+                Colors.black87,
+              ],
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: _buildContents(context),
         ),
-        child: _buildContents(context),
       ),
     );
   }
@@ -82,12 +84,11 @@ class _TrackPageState extends State<TrackPage> {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         IconButton(
-         icon:
-         const Icon(
-           Icons.arrow_back_ios,
-           color: Colors.white,
-           size: 24,
-         ),
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+            size: 24,
+          ),
           onPressed: Navigator.of(context).pop,
           padding: EdgeInsets.zero,
           constraints: const BoxConstraints(),
@@ -156,18 +157,69 @@ class _TrackPageState extends State<TrackPage> {
             ],
           ),
           IconButton(
-            icon: (manager.isLiked == true)
+            icon: (manager.isAdded == true)
                 ? const Icon(
-                    LineIcons.heart,
+                    LineIcons.plus,
                     color: Colors.green,
                     size: 27,
                   )
                 : Icon(
-                    LineIcons.heart,
+                    LineIcons.plus,
                     color: Colors.grey.shade400,
                     size: 27,
                   ),
             onPressed: manager.toggleLike,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildConnectRow() {
+    const double size = 5;
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.all(3.0),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(15)),
+          border: Border.all(color: Colors.black87)),
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(7),
+            child: const Text(
+              'Connect to your Spotify app to play songs :',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: "ProximaNova",
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                wordSpacing: 0.2,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 90,
+            width: 90,
+            child: Center(
+              child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: IconButton(
+                    icon: CircleAvatar(
+                      radius: 6 * size,
+                      backgroundColor: Colors.black,
+                      child: CircleAvatar(
+                        radius: 5.9 * size,
+                        backgroundColor: Colors.black,
+                        child: Image.asset("images/spotify_icon.png"),
+                      ),
+                    ),
+                    iconSize: 12 * size,
+                    onPressed: manager.connectToSpotifyRemote,
+                  )),
+            ),
           ),
         ],
       ),
@@ -199,12 +251,7 @@ class _TrackPageState extends State<TrackPage> {
                   : manager.setChangedSlider(),
               min: 0,
               max: manager.duration.inSeconds.toDouble(),
-              onChanged: (double value) {
-                setState(() {
-                  manager.seekToSecond(value.toInt());
-                  value = value;
-                });
-              },
+              onChanged: (value) => manager.seekToSecond(value.toInt()),
             ),
           ),
         ),
@@ -237,65 +284,65 @@ class _TrackPageState extends State<TrackPage> {
   }
 
   Widget _buildControlRow() {
-    return Container(
-      padding: const EdgeInsets.only(left: 22, right: 22),
-      width: double.infinity,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Icon(
-            LineIcons.random,
-            color: Colors.grey.shade400,
-          ),
-          const Icon(
-            Icons.skip_previous,
-            color: Colors.white,
-            size: 40,
-          ),
-          SizedBox(
-            height: 90,
-            width: 90,
-            child: Center(
-              child: IconButton(
-                iconSize: 70,
-                alignment: Alignment.center,
-                icon: manager.isPlayed
-                    ? const Icon(
-                        Icons.pause_circle_filled,
-                        color: Colors.white,
-                      )
-                    : const Icon(
-                        Icons.play_circle_filled,
-                        color: Colors.white,
-                      ),
-                onPressed: () {
-                  setState(() {
-                    manager.togglePlay();
-                    if (manager.isPlayed) {
-                      // play song
-                      print("Playing .....");
-                    } else {
-                      // stop song
-                      print("Paused .....");
-                    }
-                  });
-                },
-              ),
+    return manager.isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : Container(
+            padding: const EdgeInsets.only(left: 22, right: 22),
+            width: double.infinity,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Icon(
+                  LineIcons.random,
+                  color: Colors.grey.shade400,
+                ),
+                const Icon(
+                  Icons.skip_previous,
+                  color: Colors.white,
+                  size: 40,
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: Center(
+                    child: IconButton(
+                      iconSize: 70,
+                      alignment: Alignment.center,
+                      icon: manager.isPlayed
+                          ? const Icon(
+                              Icons.pause_circle_filled,
+                              color: Colors.white,
+                            )
+                          : const Icon(
+                              Icons.play_circle_filled,
+                              color: Colors.white,
+                            ),
+                      onPressed: () {
+                        manager.togglePlay();
+                        if (manager.isPlayed) {
+                          // play song
+                          print("Playing .....");
+                        } else {
+                          // stop song
+                          print("Paused .....");
+                        }
+                      },
+                    ),
+                  ),
+                ),
+                const Icon(
+                  Icons.skip_next,
+                  color: Colors.white,
+                  size: 40,
+                ),
+                Icon(
+                  Icons.repeat,
+                  color: Colors.grey.shade400,
+                ),
+              ],
             ),
-          ),
-          const Icon(
-            Icons.skip_next,
-            color: Colors.white,
-            size: 40,
-          ),
-          Icon(
-            Icons.repeat,
-            color: Colors.grey.shade400,
-          ),
-        ],
-      ),
-    );
+          );
   }
 
   Widget _buildBottomRow() {
@@ -336,11 +383,19 @@ class _TrackPageState extends State<TrackPage> {
               ),
               height: h * 0.48),
           SizedBox(
-            height: h * 0.05,
+            height: h * 0.04,
           ),
           _buildTitleRow(),
-          _buildSliderRow(),
-          _buildControlRow(),
+          manager.isConnected
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    _buildSliderRow(),
+                    _buildControlRow(),
+                  ],
+                )
+              : _buildConnectRow(),
           _buildBottomRow(),
         ],
       ),
