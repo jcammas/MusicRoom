968b52715e76ab8130f807856776d9eb67af5f78
Relkondo <samuel.coron@hotmail.fr>
2022-01-20 10:51:11 +0100

    removed unnecessary parts of test

--- music_room_app/test/mocks.dart
+++ music_room_app/test/mocks.dart
@@ -1,18 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:mockito/mockito.dart';
 import 'package:music_room_app/services/auth.dart';
-import 'package:music_room_app/services/database.dart';
 
 class MockAuth extends Mock implements AuthBase {}
-class MockDatabase extends Mock implements Database {}
-class MockUser extends Mock implements User {
-  MockUser();
-  factory MockUser.uid(String uid) {
-    final user = MockUser();
-    when(user.uid).thenReturn(uid);
-    return user;
-  }
-}
-class MockNavigatorObserver extends Mock implements NavigatorObserver {}
-// class MockMaterialPageRoute extends Mock implements MaterialPageRoute {}
