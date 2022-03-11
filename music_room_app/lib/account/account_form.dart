20b91c40b5af27c99a8a642786b85a00f1887558
jcammas <matoulet.cammas@gmail.com>
2022-03-11 16:48:37 +0100

    build privacy settings modal front

--- music_room_app/lib/account/account_form.dart
+++ music_room_app/lib/account/account_form.dart
@@ -28,12 +28,279 @@ class AccountForm extends StatefulWidget {
 
 class _AccountFormState extends State<AccountForm> {
   late Map<String, dynamic> settingsData;
+  bool isSwitchedEmail = false;
+  bool isSwitchedEmailWithFriends = false;
+  bool isSwitchedFriends = false;
+  bool isSwitchedFriendsWithFriends = false;
+  bool isSwitchedPlaylist = false;
+  bool isSwitchedPlaylistWithFriends = false;
 
   Map<String, dynamic> fillSettingsData(UserApp? user) {
     return <String, dynamic>{
       'Name': user == null ? 'N/A' : user.name,
       'Email': user == null ? 'N/A' : user.email,
+      'Friends': user == null ? 'N/A' : user.friends,
+      'playlist': user == null ? 'N/A' : user.playlists,
     };
+  }
+
+  _myPrivacySettings(context) {
+    showModalBottomSheet(
+        context: context,
+        constraints: BoxConstraints(
+          maxWidth: MediaQuery.of(context).size.width * 0.9,
+          maxHeight: MediaQuery.of(context).size.height * 0.8,
+        ),
+        isScrollControlled: true,
+        backgroundColor: Colors.transparent,
+        builder: (BuildContext context) {
+          return StatefulBuilder(builder: (context, StateSetter setState) {
+            return SingleChildScrollView(
+              child: Container(
+                color: Colors.transparent,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10.0),
+                      topRight: Radius.circular(10.0),
+                    ),
+                  ),
+                  alignment: Alignment.center,
+                  child: Container(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      children: <Widget>[
+                        IconButton(
+                            onPressed: () => Navigator.pop(context),
+                            icon: Icon(Icons.arrow_back)),
+                        Text(
+                          "Account Privacy",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0XFF072BB8),
+                            fontWeight: FontWeight.w900,
+                            fontStyle: FontStyle.italic,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "My e-mail",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Color(0XFF434343),
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Switch(
+                                activeTrackColor: Color(0XFF072BB8),
+                                activeColor: Color(0XFF072BB8),
+                                value: isSwitchedEmail,
+                                onChanged: (value) {
+                                  setState(() {
+                                    isSwitchedEmail = value;
+                                  });
+                                }),
+                            isSwitchedEmail
+                                ? Icon(Icons.lock, color: Color(0XFF072BB8))
+                                : SizedBox(),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "My friends list",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Color(0XFF434343),
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Switch(
+                                activeTrackColor: Color(0XFF072BB8),
+                                activeColor: Color(0XFF072BB8),
+                                value: isSwitchedFriends,
+                                onChanged: (value) {
+                                  setState(() {
+                                    isSwitchedFriends = value;
+                                  });
+                                }),
+                            isSwitchedFriends
+                                ? Icon(Icons.lock, color: Color(0XFF072BB8))
+                                : SizedBox(),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "My playlist",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Color(0XFF434343),
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Switch(
+                                activeTrackColor: Color(0XFF072BB8),
+                                activeColor: Color(0XFF072BB8),
+                                value: isSwitchedPlaylist,
+                                onChanged: (value) {
+                                  setState(() {
+                                    isSwitchedPlaylist = value;
+                                  });
+                                }),
+                            isSwitchedPlaylist
+                                ? Icon(Icons.lock, color: Color(0XFF072BB8))
+                                : SizedBox(),
+                          ],
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.05,
+                        ),
+                        Text(
+                          "Only friends can see",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0XFF072BB8),
+                            fontWeight: FontWeight.w900,
+                            fontStyle: FontStyle.italic,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02,
+                        ),
+                        isSwitchedEmail
+                            ? Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "My e-mail",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Color(0XFF434343),
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  Switch(
+                                      activeTrackColor: Color(0XFF072BB8),
+                                      activeColor: Color(0XFF072BB8),
+                                      value: isSwitchedEmailWithFriends,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          isSwitchedEmailWithFriends = value;
+                                        });
+                                      }),
+                                  isSwitchedEmailWithFriends
+                                      ? Icon(Icons.people,
+                                          color: Color(0XFF072BB8))
+                                      : SizedBox(),
+                                ],
+                              )
+                            : SizedBox(),
+                        isSwitchedFriends
+                            ? Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "My friends list",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Color(0XFF434343),
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  Switch(
+                                      activeTrackColor: Color(0XFF072BB8),
+                                      activeColor: Color(0XFF072BB8),
+                                      value: isSwitchedFriendsWithFriends,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          isSwitchedFriendsWithFriends = value;
+                                        });
+                                      }),
+                                  isSwitchedFriendsWithFriends
+                                      ? Icon(Icons.people,
+                                          color: Color(0XFF072BB8))
+                                      : SizedBox(),
+                                ],
+                              )
+                            : SizedBox(),
+                        isSwitchedPlaylist
+                            ? Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "My playlists",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Color(0XFF434343),
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  Switch(
+                                      activeTrackColor: Color(0XFF072BB8),
+                                      activeColor: Color(0XFF072BB8),
+                                      value: isSwitchedPlaylistWithFriends,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          isSwitchedPlaylistWithFriends = value;
+                                        });
+                                      }),
+                                  isSwitchedPlaylistWithFriends
+                                      ? Icon(
+                                          Icons.people,
+                                          color: Color(0XFF072BB8),
+                                        )
+                                      : SizedBox(),
+                                ],
+                              )
+                            : SizedBox(),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.05,
+                        ),
+                        ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                const Color(0XFF072BB8),
+                              ),
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                isSwitchedEmail = false;
+                                isSwitchedEmailWithFriends = false;
+                                isSwitchedFriends = false;
+                                isSwitchedFriendsWithFriends = false;
+                                isSwitchedPlaylist = false;
+                                isSwitchedPlaylistWithFriends = false;
+                              });
+                            },
+                            child: Text(
+                              "Reset privacy settings",
+                            )),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          });
+        }).whenComplete(() => setState(() {}));
   }
 
   @override
@@ -98,12 +365,18 @@ class _AccountFormState extends State<AccountForm> {
                         ),
                         SettingsTile(
                           title: 'Privacy Settings',
-                          leading: const Icon(Icons.privacy_tip),
-                          onPressed: (BuildContext context) {},
+                          leading: const Icon(
+                            Icons.privacy_tip,
+                            color: Color(0XFF072BB8),
+                          ),
+                          onPressed: (BuildContext context) {
+                            _myPrivacySettings(context);
+                          },
                         ),
                         SettingsTile(
                           title: 'Default Room Settings',
-                          leading: const Icon(Icons.music_note),
+                          leading: const Icon(Icons.music_note,
+                              color: Color(0XFF072BB8)),
                           onPressed: (BuildContext context) {},
                         ),
                         CustomSettingsTile(
